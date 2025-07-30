e5df284522a86a5dd6e8f15ffde5164ebf09c962
AresNeutron <arexneutron@gmail.com>
2025-07-30 08:34:52 -0300

    feat: custom response renderer class

--- server/expense_tracker/expense_tracker/settings.py
+++ server/expense_tracker/expense_tracker/settings.py
@@ -143,6 +143,9 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'api.renderers.CustomResponseRenderer',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
