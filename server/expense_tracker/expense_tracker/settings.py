eb4b07230efb1e5c048ecfd3f96baa7dea85b11b
AresNeutron <arexneutron@gmail.com>
2025-07-30 14:57:16 -0300

    migrating to production database

--- server/expense_tracker/expense_tracker/settings.py
+++ server/expense_tracker/expense_tracker/settings.py
@@ -80,11 +80,15 @@ WSGI_APPLICATION = "expense_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+from decouple import config
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse(
+        config('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Caching
